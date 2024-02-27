be7f8b1d2695f41cc6f8b338f9089d2cb4129d63
Sridhar-123 <kambalasridhar15@gmail.com>
2024-02-27 09:34:53 +0530

    Function

---
+++ Function.js
@@ -0,0 +1,12 @@
+let func =(x) =>{
+    console.log("Hello This is Sridhar",x);
+}
+
+func(23);
+
+function nice(name) {
+    console.log("Hello This"+name);
+    console.log("Hello "+name+"chggd");
+    console.log("My age of my name"+name);
+}
+nice(23)
