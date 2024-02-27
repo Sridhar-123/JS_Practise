02e373408e95e30a9f8415481943adc6ae7539da
Sridhar-123 <kambalasridhar15@gmail.com>
2024-02-27 09:56:50 +0530

    updation

--- Function.js
+++ Function.js
@@ -1,3 +1,5 @@
+
+// let and const mandatory funnction is not valid in arrow function
 let func =(x) =>{
     console.log("Hello This is Sridhar",x);
 }
@@ -10,3 +12,8 @@ function nice(name) {
     console.log("My age of my name"+name);
 }
 nice(23)
+
+function sum(num1,num2){
+    console.log("The sum of two number is :"+(num1+num2));
+}
+sum(2,4)
